2a375c1221d208d9dca57df34b27e09c12bf039f
Tristan <43221884+TristanJDuke@users.noreply.github.com>
2024-11-30 10:18:51 -0400

    Added Cash UI (#44)

---
+++ Assets/Scripts/UI/Shop/MoneyUIController.cs
@@ -0,0 +1,32 @@
+using UnityEngine;
+using TMPro;
+
+public class MoneyUIController : MonoBehaviour
+{
+    public TextMeshProUGUI moneyText;
+    private int currentMoney;
+
+    private void Start()
+    {
+        
+        UpdateMoneyDisplay();
+    }
+
+    private void Update()
+    {
+        int playerMoney = PlayerManager.Instance.playerData.money;
+        if (playerMoney != currentMoney)
+        {
+            currentMoney = playerMoney;
+            UpdateMoneyDisplay();
+        }
+    }
+
+    private void UpdateMoneyDisplay()
+    {
+        if (moneyText != null)
+        {
+            moneyText.text = $"${PlayerManager.Instance.playerData.money}";
+        }
+    }
+}
